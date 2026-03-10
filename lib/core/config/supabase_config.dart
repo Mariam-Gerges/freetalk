6460eb1fc65ca0bbcfee8570f1ba6a2c8b7770ef
mariam <mariamgerges204@gmail.com>
2026-03-10 23:10:28 +0200

    add init subabase and do image ui and fix some erros

--- lib/core/config/supabase_config.dart
+++ lib/core/config/supabase_config.dart
@@ -1,4 +1,4 @@
 class SupabaseConfig {
-  static const String url = "https://dvzyheloqqciepeyaslc.supabase.co";
-  static const String anonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR2enloZWxvcXFjaWVwZXlhc2xjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE3ODQzNTEsImV4cCI6MjA4NzM2MDM1MX0.2pDi7BBgPs8oiQ34r2IBj-GVyuDIfnfESOm_4gbCAyY";
+  static const String url = "https://sjiyqqhsufnejjcbakpe.supabase.co";
+  static const String anonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNqaXlxcWhzdWZuZWpqY2Jha3BlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMxNjM0MTIsImV4cCI6MjA4ODczOTQxMn0.tgAD4kxR_ZZJhYC6Z5QH8x5iLWnVyY_dQesXQHw7QL0";
 }
--- lib/feature/translate/data/model/signs_remote_data_source.dart
+++ lib/feature/translate/data/model/signs_remote_data_source.dart
@@ -1,13 +1,38 @@
-import '../../../../core/network/supabase_client.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
 
 class SignsRemoteDataSource {
-  final _client = SupabaseClientService.client;
+  // final _client = SupabaseClientService.client;
 
-  Future<List<Map<String, dynamic>>> fetchSigns() async {
-    final response = await _client
-        .from('signs')
-        .select();
+//   Future<List<Map<String, dynamic>>> fetchSigns() async {
+//     final response = await _client
+//         .from('signs')
+//         .select();
 
-    return List<Map<String, dynamic>>.from(response);
-  }
+//     return List<Map<String, dynamic>>.from(response);
+//   }
+// }
+
+
+
+
+
+
+
+// Future<void> convertWord(String word, String language) async {
+
+//   resultImages.clear();
+
+//   for (int i = 0; i < word.length; i++) {
+
+//     String letter = word[i];
+
+//     String? image = await getSignImage(letter, language);
+
+//     if (image != null) {
+//       resultImages.add(image);
+//     }
+//   }
+
+//   setState(() {});
 }
